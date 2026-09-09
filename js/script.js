fe62093a7ed7a3a938a1a7e06b1d469ea41e2e4d
Sapna Choudhary <sapnachoudhary5578@gmail.com>
2026-09-09 19:37:06 +0530

    Initial website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuButton = document.getElementById("menuButton");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuButton.addEventListener("click", function () {
+
+    mobileMenu.classList.toggle("hidden");
+
+});
